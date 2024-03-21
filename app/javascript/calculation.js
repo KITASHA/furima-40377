acea37866d2ade536ecb8b84ebd254074c021ebc
Kitagawa <140226k@gmail.com>
2024-03-21 13:33:53 +0900

    手数料計算機能実装

---
+++ app/javascript/calculation.js
@@ -0,0 +1,14 @@
+function calculation (){
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("keyup", () => {
+    const commission = Math.round(parseFloat(itemPrice.value) * 0.1);
+    const addItemPrice = document.getElementById("add-tax-price");
+    addItemPrice.innerHTML = `${commission}`;
+    
+    const salesProfit = Math.round(parseFloat(itemPrice.value) - commission);
+    const profit = document.getElementById("profit");
+    profit.innerHTML = `${salesProfit}`;
+  });
+}
+
+window.addEventListener('turbo:load', calculation);
