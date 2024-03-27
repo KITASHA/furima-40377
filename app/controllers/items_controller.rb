93de49a4583bc885d57cafde019acda297f88b09
Kitagawa <140226k@gmail.com>
2024-03-27 15:24:20 +0900

    詳細画面の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    if @item.user_id!= current_user.id
+    if @item.user_id != current_user.id || @item.order.present?
       redirect_to root_path
     end
   end
