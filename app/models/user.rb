a49021ab8c67d3d68b4c2772ec0f41c40ff5214f
Kitagawa <140226k@gmail.com>
2024-03-27 18:12:01 +0900

    ニックネームのバリデーション削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 
   has_many :items
   has_many :orders
 
-  validates :nickname, presence: true, format: { with: /\A[ぁ-んァ-ヶａ-ｚＡ-Ｚ０-９一-龥々]+\z/ }
+  validates :nickname, presence: true
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶａ-ｚＡ-Ｚ０-９一-龥々]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶａ-ｚＡ-Ｚ０-９一-龥々]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
